3f946e70c02814a0eabcf8c46cbd7dcff3ad18f3
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-10 09:45:23 +0100

    added addition script

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 alert('Welcome to the JS Calculator! \nClick OK to get started.');
 var action = prompt('Please select which calculation to perform: \nFor Addition, please enter "a". \nFor Subtraction, please enter "s". \nFor Addition, please enter "a". \nFor Multplication, please enter "m". \nFor Division, please enter "s". \nFor squaring a number, please enter "sq". \nFor Powers, please enter "p". \n').toLowerCase();
 if (action==='a') {
-
+	var number1 = parseFloat(prompt('You have selected Addition. \n Please enter your first number.'));
+	var number2 = parseFloat(prompt('Please enter your second number'));
+	var result = number1 + number2;
+	alert('Your result is: \n' + number1 + ' + ' + number2 + ' = ' + result);
 } else if (action==='s') {
 
 } else if (action==='m') {
