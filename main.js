76ebef2e52226890c478965ff6491f42661284bc
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-10 09:21:20 +0100

    added initial calculation selection

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 alert('Welcome to the JS Calculator! \nClick OK to get started.');
+var action = prompt('Please select which calculation to perform: \nFor Addition, please enter "a". \nFor Subtraction, please enter "s". \nFor Addition, please enter "a". \nFor Multplication, please enter "m". \nFor Division, please enter "s". \nFor squaring a number, please enter "sq". \nFor Powers, please enter "p". \n').toLowerCase();
+if (action==='a') {
+
+} else if (action==='s') {
+
+} else if (action==='m') {
+
+} else if (action==='d') {
+
+} else if (action==='sq') {
+
+} else if (action==='p') {
+
+} else {
+	alert('Error. You did not enter one of the possible commands. \nPlease refresh the page to try again');
+}
